8d3f3e8197249d62c989ff5a990c521e2da37d14
Ionut Traistaru <ionut.m.traistaru@gmail.com>
2019-11-13 14:14:40 +0000

    switch to firestore

--- bot/db.js
+++ bot/db.js
@@ -5,31 +5,29 @@ firebase.initializeApp({
     credential: firebase.credential.cert(config),
     databaseURL: 'https://ig-bot-1569a.firebaseio.com'
 });
-const database = firebase.database();
 
-const following = (param = '') => database.ref(`following/${param}`);
+const db = firebase.firestore();
 
-const followHistory = (param = '') => database.ref(`follow_history/${param}`);
+const followingRef = db.collection('following');
+const followHistoryRef = db.collection('follow-history');
 
 const addFollowing = async username => {
-    const added = new Date().getTime();
-    return following(username).set({ username, added });
+    const added = new Date();
+    const user = { username, added };
+    return await followingRef.doc(username).set(user);
 };
 
 const getFollowings = async () =>
-    following()
-        .once('value')
-        .then(data => data.val());
+    followingRef.get().then(snapshot => snapshot.forEach(doc => doc.data()));
 
 const unFollow = async username =>
-    following(username)
-        .remove()
-        .then(() => followHistory(username).set({ username }));
+    followingRef
+        .doc(username)
+        .delete()
+        .then(() => followHistoryRef.add({ username }));
 
 const inHistory = async username =>
-    followHistory(username)
-        .once('value')
-        .then(data => data.val());
+    followHistoryRef.get().then(snapshot => snapshot.forEach(doc => doc.data()));
 
 module.exports = {
     addFollowing,
